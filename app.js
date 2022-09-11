142c6ba2069b501360c60e37c064e55a4bd797a1
Nooruz Zaman <53435059+mdNzaman@users.noreply.github.com>
2022-09-11 14:48:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ const express = require("express");
 const app = express();
 const router = require("./routers/routes");
 const { Server } = require("socket.io");
-const port = process.env.PORT || 4000;
-const saveChatMessageModel = require("./models/saveChatMessageModel");
-const notificationToReceiverController = require ("./controllers/notificationToReceiverController");
+const port = process.env.PORT
 var cron = require('node-cron');
-const { expireHistoricalData } = require("./controllers/getHistoricalDataController");
 
 app.use(express.json());
 
@@ -18,12 +15,12 @@ app.use(express.json());
 
 var socketserver = require('http').createServer();
 
-const io = new Server(socketserver, {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-    },
-  });
+// const io = new Server(socketserver, {
+//     cors: {
+//       origin: "*",
+//       methods: ["GET", "POST"],
+//     },
+//   });
   
 app.get("/",(req,res)=>{
     console.log("apis working..");
@@ -33,54 +30,9 @@ app.get("/",(req,res)=>{
     });
 });
 
-// app.use("/flok22/app/v1/",router);
 app.use("/api",router);
 
-var roomno = 1;
-
- const connnections = [];
-
-io.on('connection', (socket) => {
-    console.log('New user connected');
-
-
-    socket.on('join', function(data) {
-        console.log("User Joined");
-        if (data.chat_id != null){
-            roomno = data.chat_id;
-
-            console.log("room num",roomno);
-            socket.join("room-" + roomno);
-        }      
-    });
-
-     connnections.push(socket);
-
-    socket.on('send_message', (data) => {
-        var message_id = 0;
-        var sender_id = data.sender_id;
-        var receiver_id = data.receiver_id;
-        var message = data.message;
-        var chat_id = data.chat_id;
-        var created_datetime = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-        console.log("chat_id",chat_id);
-        io.sockets.in("room-"+chat_id).emit('receive_message', {message : data.message, sender_id: sender_id, receiver_id:receiver_id, messaged_at: created_datetime});
-        saveChatMessageModel.saveChatMessage(chat_id, sender_id, receiver_id, message, created_datetime, function (err,result) {
-            if(err){
-                console.log('message_error'+err);
-            } else{               
-                var message_id = result.insertId;               
-                notificationToReceiverController.notificationToReceiver(sender_id, receiver_id, chat_id, message_id);
-                console.log('succees_message ' + message_id);
-            }
-        });
-        
-       console.log({message : data.message, receiver_id: receiver_id, sender_id: sender_id, messaged_at: created_datetime, message_id: message_id});
-
-    });
-});
-
-socketserver.listen(4010); // Socket.IO, port 3001
+// socketserver.listen(3210); // Socket.IO, port 4211
 
 app.listen(port,()=>{
     console.log(`server on and running at port: ${port}`);
